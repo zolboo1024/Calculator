91be723e4fe7d3363d7a554235209df0fdbb601c
zolboo1024 <erdenebz@dickinso.edu>
2020-10-11 08:19:43 -0500

    renamediv to divie to be more descriptive

--- Calculator.java
+++ Calculator.java
@@ -58,7 +58,7 @@ public class Calculator {
 	 *            a double
 	 * @return x/y
 	 */
-	public double div(double x, double y) {
+	public double divide(double x, double y) {
 		return y / x;
 	}
 
